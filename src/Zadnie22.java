ab8d937e1ea96c5a0b62bfb040ae04514797c46b
DZ13NC10L <89974032+kstozek@users.noreply.github.com>
2023-03-24 10:30:25 +0100

    Update Zadnie22.java

--- src/Zadnie22.java
+++ src/Zadnie22.java
@@ -18,3 +18,59 @@ public class Zadnie22 {
         System.out.println("Wartość prowizji zapłaconej przy sprzedaży: " + zysk + " zł");
     }
 }
+
+
+
+
+
+import java.util.Scanner;
+
+public class Biegacze {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Pytanie o nazwiska biegaczy i ich czasy
+        System.out.println("Podaj nazwisko i czas ukończenia biegu dla każdego z trzech biegaczy:");
+        System.out.print("Biegacz 1: ");
+        String nazwisko1 = scanner.next();
+        int czas1 = scanner.nextInt();
+        System.out.print("Biegacz 2: ");
+        String nazwisko2 = scanner.next();
+        int czas2 = scanner.nextInt();
+        System.out.print("Biegacz 3: ");
+        String nazwisko3 = scanner.next();
+        int czas3 = scanner.nextInt();
+        
+        // Wyświetlenie nazwisk biegaczy zgodnie z kolejnością na mecie
+        System.out.println("Kolejność na mecie:");
+        if (czas1 < czas2 && czas1 < czas3) {
+            System.out.println(nazwisko1);
+            if (czas2 < czas3) {
+                System.out.println(nazwisko2);
+                System.out.println(nazwisko3);
+            } else {
+                System.out.println(nazwisko3);
+                System.out.println(nazwisko2);
+            }
+        } else if (czas2 < czas1 && czas2 < czas3) {
+            System.out.println(nazwisko2);
+            if (czas1 < czas3) {
+                System.out.println(nazwisko1);
+                System.out.println(nazwisko3);
+            } else {
+                System.out.println(nazwisko3);
+                System.out.println(nazwisko1);
+            }
+        } else {
+            System.out.println(nazwisko3);
+            if (czas1 < czas2) {
+                System.out.println(nazwisko1);
+                System.out.println(nazwisko2);
+            } else {
+                System.out.println(nazwisko2);
+                System.out.println(nazwisko1);
+            }
+        }
+    }
+}
+
